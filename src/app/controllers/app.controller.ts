778fd79acf65026b23e075aca33ee957d473f1b1
Dalia <matheusdalia8@gmail.com>
2023-06-14 19:03:12 -0300

    swagger-feature

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from '@/app/services/app.service';
-
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+@ApiTags('Your API Tags')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @ApiOperation({ summary: 'Summary of your API operation' })
+  @ApiResponse({ status: 200, description: 'Success message' })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { AppModule } from '@/app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug', 'log', 'verbose'],
   });
+  // Create a Swagger document
+  const config = new DocumentBuilder()
+    .setTitle('Scrap Money')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  // Generate Swagger JSON from the document
+  const document = SwaggerModule.createDocument(app, config);
+
+  // Set up Swagger UI
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000).then(() => {
     // Use a package emoji to indicate that the server is running
     console.log('📦 Server running on port 3000');
